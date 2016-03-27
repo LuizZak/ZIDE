314ab7bc946bfd8df2b777554663930d80117499
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2016-03-27 19:23:29 -0300

    Minor whitespace changes

--- ZIDE/Views/Controls/ProjectTreeForm.cs
+++ ZIDE/Views/Controls/ProjectTreeForm.cs
@@ -23,6 +23,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 #endregion
+
 using WeifenLuo.WinFormsUI.Docking;
 
 namespace ZIDE.Views.Controls
--- ZIDE/Views/Controls/ScriptDocumentForm.cs
+++ ZIDE/Views/Controls/ScriptDocumentForm.cs
@@ -23,6 +23,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 #endregion
+
 using System;
 using ICSharpCode.TextEditor;
 using ICSharpCode.TextEditor.Document;
